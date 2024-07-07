d4bba3f49dfd6cc8612a8a0a1195b241347068c8
cwhite70 <chase.t.white@hotmail.com>
2024-07-07 15:06:08 -0600

    adding currency package, styling, search functionality

--- app/ui/navbar/NavLinks.tsx
+++ app/ui/navbar/NavLinks.tsx
@@ -1,22 +1,34 @@
+"use client";
+
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
+import { RiHome5Fill } from "react-icons/ri";
+import { TbStack2Filled } from "react-icons/tb";
+import { usePathname } from "next/navigation";
 
 const NavLinks = () => {
+  const path = usePathname();
+
   return (
-    <ul className="flex gap-6">
-      <li className="flex gap-2.5">
-        <Image src="/images/Home.svg" alt="Home icon" width={20} height={20} />
+    <ul className="flex gap-6 font-medium">
+      <Link
+        href={"/"}
+        className={`flex items-center gap-2.5 px-4 py-3  ${
+          path === "/" ? "text-white" : "text-[#FFFFFF80]"
+        }`}
+      >
+        <RiHome5Fill className="w-5 h-5" />
         Home
-      </li>
-      <li className="flex gap-2.5">
-        <Image
-          src="/images/Portfolio.svg"
-          alt="Portfolio icon"
-          width={20}
-          height={20}
-        />
+      </Link>
+      <Link
+        href={"/portfolio"}
+        className={`flex items-center gap-2.5 px-4 py-3 ${
+          path === "/portfolio" ? "text-white" : "text-[#FFFFFF80]"
+        }`}
+      >
+        <TbStack2Filled className="w-5 h-5" />
         Portfolio
-      </li>
+      </Link>
     </ul>
   );
 };
--- app/ui/navbar/buttons.tsx
+++ app/ui/navbar/buttons.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import Image from "next/image";
 import { useTheme } from "next-themes";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 export function ThemeButton() {
   const { theme, setTheme } = useTheme();
 
   return (
     <button
-      className="flex items-center justify-center p-3.5 bg-dark-purple-700 rounded-md border border-[#ffffff0d]"
+      className="flex items-center justify-center p-3.5"
       title="Toggle theme"
       data-theme={`${theme}`}
       onClick={() => setTheme(() => (theme === "light" ? "dark" : "light"))}
     >
-      <Image
+      {theme === "light" ? (
+        <FiSun className="w-5 h-5" data-hide-on-theme="dark" />
+      ) : (
+        <FiMoon className="w-5 h-5" data-hide-on-theme="light" />
+      )}
+      {/* <Image
         src="/images/Light.svg"
         alt="Light icon"
         width={20}
@@ -26,7 +31,7 @@ export function ThemeButton() {
         width={20}
         height={20}
         data-hide-on-theme="light"
-      />
+      /> */}
     </button>
   );
 }
