ce9c2add10fe704293c19bf967ec649c5cc033a8
cwhite70 <chase.t.white@hotmail.com>
2024-07-07 11:31:29 -0600

    styles update

--- app/ui/portfolio/InvestmentCalculator.tsx
+++ app/ui/portfolio/InvestmentCalculator.tsx
@@ -115,20 +115,30 @@ const InvestmentCalculator = ({
           <div className="flex justify-between mb-4 text-birches-100">
             {/* date inputs */}
             <div className="flex gap-4">
-              <div className="flex p-2 bg-[#191932] rounded-lg">
-                <input type="datetime-local" name="" id="" />
+              <div className="flex gap-4 p-2 bg-[#191932] rounded-lg">
+                <input
+                  className="bg-transparent cursor-pointer"
+                  type="datetime-local"
+                  name=""
+                  id=""
+                />
                 <button className="text-white">
                   <BsQuestionCircle />
                 </button>
               </div>
-              <div className="flex p-2 bg-[#191932] rounded-lg">
-                <input type="datetime-local" name="" id="" />
+              <div className="flex gap-4 p-2 bg-[#191932] rounded-lg">
+                <input
+                  className="bg-transparent cursor-pointer"
+                  type="datetime-local"
+                  name=""
+                  id=""
+                />
                 <button className="text-white">
                   <BsQuestionCircle />
                 </button>
               </div>
             </div>
-            <div>
+            <div className="px-6 py-2 bg-[#191932] rounded-lg">
               <button>
                 {/* No idea what this button is for */}
                 Q-ty
