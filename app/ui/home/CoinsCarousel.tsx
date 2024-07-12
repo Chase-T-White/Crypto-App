28b48ba76570be7ce36003a9f5ac19133a8fc341
cwhite70 <chase.t.white@hotmail.com>
2024-07-11 19:08:23 -0600

    Carousel response/theme designs

--- app/ui/home/CoinsCarousel.tsx
+++ app/ui/home/CoinsCarousel.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-import CoinButton from "./CoinButton";
+import { useState, useCallback } from "react";
 import { useSelector } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { useResizeDetector } from "react-resize-detector";
+import CoinButton from "./CoinButton";
+import { CoinCarouselSkeleton } from "../skeletons";
+import { capitalizeFirstLetter } from "@/utils/formatText";
 import { selectCoinIds } from "@/lib/features/charts/chartSlice";
 import { coinsFetchStatus } from "@/lib/features/coins/coinsSlice";
-import { ErrorBoundary } from "react-error-boundary";
-import { CoinCarouselSkeleton } from "../skeletons";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { capitalizeFirstLetter } from "@/utils/formatText";
 
 const CoinsCarousel = ({
   setCoinFetchById,
@@ -18,14 +19,40 @@ const CoinsCarousel = ({
   coinDataError: boolean;
 }) => {
   const [carouselIndex, setCarouselIndex] = useState(0);
+  const [coinsPerGroup, setCoinsPerGroup] = useState(5);
   const coinIds = useSelector(selectCoinIds);
   const coinsStatus = useSelector(coinsFetchStatus);
 
-  let groupedCoinDisplay = [];
+  const onResize = useCallback(() => {
+    const windowWidth = window.innerWidth;
+    console.log(windowWidth);
 
-  for (let i = 0; i < coins.length; i += 5) {
+    if (windowWidth > 1280) {
+      if (coinsPerGroup === 5) return;
+      setCoinsPerGroup(5);
+    } else if (windowWidth > 1024) {
+      if (coinsPerGroup === 4) return;
+      setCoinsPerGroup(4);
+    } else if (windowWidth > 768) {
+      if (coinsPerGroup === 3) return;
+      setCoinsPerGroup(3);
+    } else {
+      if (coinsPerGroup === 2) return;
+      setCoinsPerGroup(2);
+    }
+  }, [coinsPerGroup]);
+
+  const { ref } = useResizeDetector({
+    handleHeight: false,
+    refreshMode: "debounce",
+    refreshRate: 1000,
+    onResize,
+  });
+
+  let groupedCoinDisplay = [];
+  for (let i = 0; i < coins.length; i += coinsPerGroup) {
     let groupedCoins = [];
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < coinsPerGroup && i + j < coins.length; j++) {
       groupedCoins.push(coins[i + j]);
     }
     groupedCoinDisplay.push(groupedCoins);
@@ -38,14 +65,14 @@ const CoinsCarousel = ({
           <CoinCarouselSkeleton />
         ) : (
           <>
-            <div className="relative h-[78px]">
+            <div className="relative h-full">
               {coinDataError && (
                 <span className="absolute -top-[100%] translate-y-1/2 left-1/2 -translate-x-1/2 text-red">
                   Only two coins can be compared at a time. First unselect a
                   coin.
                 </span>
               )}
-              <div className="w-full overflow-x-hidden">
+              <div className="w-full h-full overflow-x-hidden" ref={ref}>
                 <ul className="absolute w-full flex gap-2">
                   {groupedCoinDisplay[carouselIndex].map((coin, i) => {
                     const active = coinIds.includes(
@@ -63,7 +90,7 @@ const CoinsCarousel = ({
             </div>
             {carouselIndex > 0 && (
               <button
-                className="absolute w-[48px] aspect-square rounded-full active-button flex items-center justify-center top-1/2 -translate-y-1/2 -left-10"
+                className="absolute w-[48px] aspect-square rounded-full active-button flex items-center justify-center top-1/2 -translate-y-1/2 -left-[24px]"
                 onClick={() => setCarouselIndex(carouselIndex - 1)}
               >
                 <FiChevronLeft className="w-[1.75rem] h-[3.5rem]" />
@@ -71,7 +98,7 @@ const CoinsCarousel = ({
             )}
             {carouselIndex < groupedCoinDisplay.length - 1 && (
               <button
-                className="absolute w-[48px] aspect-square rounded-full active-button flex items-center justify-center top-1/2 -translate-y-1/2 -right-10"
+                className="absolute w-[48px] aspect-square rounded-full active-button flex items-center justify-center top-1/2 -translate-y-1/2 -right-[24px]"
                 onClick={() => setCarouselIndex(carouselIndex + 1)}
               >
                 <FiChevronRight className="w-[1.75rem] h-[3.5rem]" />
